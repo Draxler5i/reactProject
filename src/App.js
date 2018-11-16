777ad63ac381a73ee3fdbb77d8281550c00dd7b8
Daniel Merida <daniel.merida.avantica@outlook.com>
2018-11-16 11:49:58 -0400

    First change in the main page

--- src/App.js
+++ src/App.js
@@ -7,19 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <h1>Wellcome to my first app on React</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div>
+          <p>
+            I am all powerfull!! xD
+          </p>
+        </div>
       </div>
     );
   }
