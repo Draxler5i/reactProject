ad9df3ba91cea6ee306aecc8429d7ef5b8f0cf06
Daniel Merida <daniel.merida.avantica@outlook.com>
2018-11-20 10:46:39 -0400

    New structure of folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+      <h1>Hello react!</h1>
       </div>
     );
   }
